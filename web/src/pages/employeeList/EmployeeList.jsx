cc8ee7c8216ecb269828d937bd050f6d265f894c
Eduardo Cavazin <eduardotorcav@gmail.com>
2024-08-14 22:24:48 -0300

    Feat(EmployeeList): Criação da página e um esqueleto inicial. Feat-11-US

---
+++ web/src/pages/employeeList/EmployeeList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './EmployeeList.css';
+import { Card } from "primereact/card";
+import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
+        
+
+function EmployeeList() {
+    return (
+        <div className="background-container flex flex-colum justify-content-center">
+            <Card title="Employee List" className="card-container flex flex-colum  justify-content-center">
+                
+            </Card>
+        </div>
+    );
+};
+
+export default EmployeeList;
