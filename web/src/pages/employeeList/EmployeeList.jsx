02678e935214df57a6e17e32834364c00605cc23
Eduardo Cavazin <eduardotorcav@gmail.com>
2024-08-18 18:50:03 -0300

    Feat(EmployeeList): Criação de arquivo .json para testes e busca do mesmo na página. Feat-11-US

--- web/src/pages/employeeList/EmployeeList.jsx
+++ web/src/pages/employeeList/EmployeeList.jsx
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './EmployeeList.css';
 import { Card } from "primereact/card";
-import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
-        
+import { ProgressSpinner } from 'primereact/progressspinner';
+import { Divider } from 'primereact/divider';
+import { DataView } from 'primereact/dataview';
+import { Avatar } from 'primereact/avatar';
 
 function EmployeeList() {
+    const [employees, setEmployees] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/archives/employeeList.json");
+                const data = await response.json();
+                setEmployees(data);
+            } catch (error) {
+                console.error("Error fetching the employee data:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const itemTemplate = (employee) => {
+        return (
+            <Card className="card-container">
+                <Avatar icon="pi pi-user" size="large" shape="square" />
+                <div>
+                    <div>{employee.nome}</div>
+                    <div>SIAPE: {employee.siape}</div>
+                    <div>{employee.cargo}</div>
+                </div>
+                <Divider />
+            </Card>
+        );
+    };
+
+    if (loading) {
+        return <div className="spinner-container"><ProgressSpinner /></div>;
+    }
+
     return (
-        <div className="background-container flex flex-colum justify-content-center">
-            <Card title="Employee List" className="card-container flex flex-colum  justify-content-center">
-                
+        <div className="background-container flex flex-column justify-content-center align-items-center">
+            <Card title='Lista de Funcionários'>
+            <DataView 
+                value={employees} 
+                layout="grid" 
+                itemTemplate={itemTemplate} 
+                paginator
+                rows={9}
+            />
             </Card>
         </div>
     );
-};
+}
 
 export default EmployeeList;
