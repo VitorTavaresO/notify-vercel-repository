6a25f7e29967f9f935c69f2eaa0d96256ee0eb2d
rSanches35 <rafaelsanches.s35@gmail.com>
2024-08-22 18:00:17 -0300

    checkpoint(Header) Estrutura inicial. Importação e primeiras modificações da Menubar. Definida as opções corretamente e posicionamento da foto de perfil.
    PL-#6
    dev

--- web/src/components/header/Header.jsx
+++ web/src/components/header/Header.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import './Header.css';
 
+import { Avatar } from 'primereact/avatar';
+import { Menubar } from 'primereact/menubar';
+
+
 const Header = () => {
+
+    const items = [
+        {label: 'Início'},{label: 'Comunicados'},{label: 'Responsáveis'},{label: 'Servidores'}
+    ];
+
+    const start = <img alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" height="40" className="ml-2"></img>;
+    const end = <Avatar image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png" className="mr-5"/>;
 
     return (
         <>
+        <Menubar model={items} start={start} end={end} />
         </>
     );
 }; export default Header;
