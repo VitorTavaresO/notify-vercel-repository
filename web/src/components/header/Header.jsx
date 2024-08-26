cb6427bc1fa37bfd8f3335599137dac9fab1b675
rSanches35 <rafaelsanches.s35@gmail.com>
2024-08-26 09:08:38 -0300

    style(Header) Refatoração do Menu. Correção do problema de scroll do Menu Dropdown de Perfil, substituindo o Menu - PrimeReact, por uma div comum.
    PL-#6
    dev

--- web/src/components/header/Header.jsx
+++ web/src/components/header/Header.jsx
@@ -1,24 +1,40 @@
-import React, { useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './Header.css';
 
 import { Menu } from 'primereact/menu';
 import { Avatar } from 'primereact/avatar';
+import { Button } from 'primereact/button';
 import { Menubar } from 'primereact/menubar';
 
 
 const Header = () => {
 
-    const menu = useRef(null);
     const items = [
         {label: 'Início' },
         {label: 'Comunicados'},
         {label: 'Responsáveis'},
         {label: 'Servidores'},
     ];
+
+    const [isUserMenuVisible, setIsUserMenuVisible] = useState(false);
+    const userMenuRef = useRef(null);
     const userMenuItems = [
         {label: 'Perfil'},
         {label: 'Sair'},
     ];
+
+    const toggleUserMenu = () => { setIsUserMenuVisible(prevState => !prevState);};
+
+    const handleClickOutside = (event) => {
+
+        if (userMenuRef.current && !userMenuRef.current.contains(event.target)) { setIsUserMenuVisible(false);}
+    };
+
+    useEffect(() => {
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => { document.removeEventListener('mousedown', handleClickOutside); };
+    }, []);
 
     const start = (
         <div className="flex align-items-center ml-3 mr-3 logo">
@@ -26,12 +42,18 @@ const Header = () => {
         </div>
     );
     const end = (
-        <div className="flex align-items-center mr-5">
+        <div className="flex align-items-center mr-3 relative" ref={userMenuRef}>
             <Avatar 
                 image="https://primefaces.org/cdn/primereact/images/avatar/asiyajavayant.png" 
-                onClick={(e) => menu.current.toggle(e)} 
+                className="mr-2"         
+                onClick={toggleUserMenu}
             />
-            <Menu model={userMenuItems} popup ref={menu} className="mt-1"/>
+            <Button icon="pi pi-chevron-down" className="p-button-text -ml-2 dropdown-icon" onClick={toggleUserMenu} />
+            {isUserMenuVisible && ( <div className="user-menu">
+                {userMenuItems.map((item, index) => (
+                    <div key={index} className="user-menu-item"> {item.label} </div>
+                ))}
+            </div> )}
         </div>
     );
 
