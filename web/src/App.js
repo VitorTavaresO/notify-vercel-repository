4459e006b0602b537eb93d36b94cd74103b97954
Eduardo Cavazin <eduardotorcav@gmail.com>
2024-08-26 13:21:34 -0300

    Feat(Employee List): Criada a rota de navegação para a página de servidores Feat-11-US

--- web/src/App.js
+++ web/src/App.js
@@ -6,6 +6,7 @@ import DefaultLayout from './components/DefaultLayout';
 import SimpleLayout from './components/SimpleLayout';
 
 import Home from './pages/home/Home';
+import EmployeeList from './pages/employeeList/EmployeeList';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<DefaultLayout><Home/></DefaultLayout>} />
           <Route path='/page1' element={<DefaultLayout><Home/></DefaultLayout>} />
           <Route path='/page2' element={<SimpleLayout><Home/></SimpleLayout>} />
+          <Route path='/employee-list' element={<DefaultLayout><EmployeeList/></DefaultLayout>} />
         </Routes>
       </BrowserRouter>
     </>
