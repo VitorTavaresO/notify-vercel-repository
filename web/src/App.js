b8293f7bba563140b55c64710f2173c443ee4c02
VitorTavaresO <vitortavares.o55@gmail.com>
2024-08-27 10:36:28 +0100

    Fix - Merge Conflicts Corrections

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,42 @@
-import './App.css';
-import Register from './pages/register/register.jsx';
+import "./App.css";
+import Register from "./pages/register/register.jsx";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DefaultLayout from "./components/DefaultLayout";
+import SimpleLayout from "./components/SimpleLayout";
 
-import DefaultLayout from './components/DefaultLayout';
-import SimpleLayout from './components/SimpleLayout';
-
-import Home from './pages/home/Home';
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<DefaultLayout><Home/></DefaultLayout>} />
-          <Route path='/page1' element={<DefaultLayout><Home/></DefaultLayout>} />
-          <Route path='/register' element={<SimpleLayout><Register/></SimpleLayout>} />
+          <Route
+            path="/"
+            element={
+              <DefaultLayout>
+                <Home />
+              </DefaultLayout>
+            }
+          />
+          <Route
+            path="/page1"
+            element={
+              <DefaultLayout>
+                <Home />
+              </DefaultLayout>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <SimpleLayout>
+                <Register />
+              </SimpleLayout>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
