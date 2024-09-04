7dc0388bc959e4e54de7d7c1433ba6c9f2674aeb
Eduardo Cavazin <eduardotorcav@gmail.com>
2024-09-04 20:03:09 -0300

    Style(Profile): Correção da tela, em conformidade com o design

--- web/src/pages/profile/Profile.jsx
+++ web/src/pages/profile/Profile.jsx
@@ -4,13 +4,14 @@ import { Card } from 'primereact/card';
 import { Avatar } from 'primereact/avatar';
 import { Button } from 'primereact/button';
 import { Tag } from 'primereact/tag';
+import { Divider } from 'primereact/divider';
 
 
 const ProfileCard = () => {
 
 
-    return (
-        <div className='container'>
+    return (    
+        <div className="container">
             <Card className="profile-card">
                 <div className="profile-header">
                     <Avatar image="/images/profile-picture.png" size="xlarge" shape="circle" className="profile-avatar" />
@@ -26,48 +27,53 @@ const ProfileCard = () => {
                     </div>
                 </div>
 
-                <div className="profile-details">
-                    <div className="detail-item">
-                        <strong>Cargo</strong>
-                        <p>CargoDoServidor</p>
+                <Divider/>
+
+                <div className="p-d-flex p-flex-wrap">
+                    <div className="profile-details p-mr-3">
+                        <div className="detail-item">
+                            <strong>Cargo</strong>
+                            <p>CargoDoServidor</p>
+                        </div>
+                        <div className="detail-item">
+                            <strong>CPF</strong>
+                            <p>000.000.000-00</p>
+                        </div>
+                        <div className="detail-item">
+                            <strong>Telefone</strong>
+                            <p>(00) 00000-0000</p>
+                        </div>
+                        <div className="detail-item">
+                            <strong>E-Mail</strong>
+                            <p>EmailDoServidor@ifpr.edu.br</p>
+                        </div>
                     </div>
-                    <div className="detail-item">
-                        <strong>CPF</strong>
-                        <p>000.000.000-00</p>
-                    </div>
-                    <div className="detail-item">
-                        <strong>E-Mail</strong>
-                        <p>EmailDoServidor@ifpr.edu.br</p>
-                    </div>
-                    <div className="detail-item">
-                        <strong>Telefone</strong>
-                        <p>(00) 00000-0000</p>
+                    <Divider layout='vertical' />
+                    <div className="communications p-mr-3">
+                        <h3>Últimos Comunicados:</h3>
+                        <div className="communication-item">
+                            <h4>Título do Comunicado nº1</h4>
+                            <p>Para: Todos</p>
+                            <p>Data: 08/08/2024</p>
+                            <p>DescriçãoDoComunicado...</p>
+                        </div>
+                        <div className="communication-item">
+                            <h4>Título do Comunicado nº2</h4>
+                            <p>Para: 1º Ano</p>
+                            <p>Data: 22/08/2024</p>
+                            <p>DescriçãoDoComunicado...</p>
+                        </div>
+                        <Button label="Ver Todos" className="p-button-link" />
                     </div>
                 </div>
-
+                <Divider />
                 <div className="profile-footer">
                     <Button label="Alterar Senha" className="p-button-danger" />
                     <Button label="Editar Perfil" className="p-button-primary" />
                 </div>
-
-                <div className="communications">
-                    <h3>Últimos Comunicados:</h3>
-                    <div className="communication-item">
-                        <h4>Título do Comunicado nº1</h4>
-                        <p>Para: Todos</p>
-                        <p>Data: 08/08/2024</p>
-                        <p>DescriçãoDoComunicado...</p>
-                    </div>
-                    <div className="communication-item">
-                        <h4>Título do Comunicado nº2</h4>
-                        <p>Para: 1º Ano</p>
-                        <p>Data: 22/08/2024</p>
-                        <p>DescriçãoDoComunicado...</p>
-                    </div>
-                    <Button label="Ver Todos" className="p-button-link" />
-                </div>
             </Card>
         </div>
+
     );
 };
 
