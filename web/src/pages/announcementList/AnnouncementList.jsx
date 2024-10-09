cf35bd24f244c3cd005cd553d429296175ce14e0
rSanches35 <rafaelsanches.s35@gmail.com>
2024-10-09 20:04:38 -0300

    feat(AnnouncementList) Estrutura da Página. Criação estrutural de Comunicados, definição da rota e navegação pelo Header.
    PL-#22
    dev

---
+++ web/src/pages/announcementList/AnnouncementList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Helmet } from 'react-helmet';
+
+import './AnnouncementList.css';
+
+const AnnouncementList = () => {
+
+    return (
+        <>
+            <Helmet>
+                <title>Comunicados</title>
+            </Helmet>
+        </>
+    );
+}; export default AnnouncementList;
