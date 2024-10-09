7cf758695d94da05c2e2ac026100bf410d3985f3
CaueYanagihara <caueyanagihara@gmail.com>
2024-10-09 20:19:27 -0300

    Checkpoint: Iniciado a comunicação com o BD.

--- backend/src/main/java/com/auction/backend/model/User.java
+++ backend/src/main/java/com/auction/backend/model/User.java
@@ -16,4 +16,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String cpf;
+    private String siape;
+    private String position;
+    private String email;
+    private String phone;
+    private String password;
 }
---
+++ web/src/pages/register/register.js
@@ -0,0 +1,45 @@
+const form = document.querySelector("form");
+const iname = document.querySelector("name");
+const icpf = document.querySelector("cpf");
+const isiape = document.querySelector("siape");
+const iposition = document.querySelector("position");
+const iemail = document.querySelector("email");
+const iphone = document.querySelector("phone"); 
+const ipassword = document.querySelector("password");
+
+function register() {
+    fetch("http://localhost:3304/api/user"), {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            name: iname.value,
+            cpf: icpf.value,
+            siape: isiape.value,
+            position: iposition.value,
+            email: iemail.value,
+            phone: iphone.value,
+            password: ipassword.value
+        })
+            .then(function (res) { console.log(res) })
+            .catch(function (res) { console.log(res) })
+    }
+}
+
+function clear() {
+    iname.value = "",
+    icpf.value = "",
+    isiape.value = "",
+    iposition.value = "",
+    iemail.value = "",
+    iphone.value = "",
+    ipassword.value = ""
+}
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    register();
+    clear();
+});
