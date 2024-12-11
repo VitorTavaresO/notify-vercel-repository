5a64e2f8e149eae3fa5916c30bdd2ae2436f0739
Vitor Tavares <vitortavares.o55@gmail.com>
2024-12-10 21:02:14 -0300

    Delete web directory

--- web/src/components/PrivateRouter.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRouter = () => {
-  const isAuthenticaded = localStorage.getItem("user");
-  return isAuthenticaded ? <Outlet /> : <Navigate to="/login" />;
-};
-export default PrivateRouter;
